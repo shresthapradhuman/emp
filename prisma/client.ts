ffb16a146c5349c08b8fb868faef6e724bf3628e
shresthapradhuman <shresthapradhuman2018@gmail.com>
2025-05-20 23:27:27 +0900

    Remove the use of the @prisma/extension-accelerate in the Prisma client initialization, simplifying the client setup while maintaining functionality in non-production environments.

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,11 +1,9 @@
 import config from '@/lib/config'
 import { PrismaClient } from './generated/prisma'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
+const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (config.nodeEnv !== 'production') globalForPrisma.prisma = prisma
 
